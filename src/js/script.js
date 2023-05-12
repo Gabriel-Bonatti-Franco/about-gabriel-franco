4b7f479d614c9fda5be414a4e9697d559e0090a8
Gabriel Franco <gabrielbonatti12@gmail.com>
2023-05-12 18:36:11 -0300

    Colocando o menu para funcionar

--- src/js/script.js
+++ src/js/script.js
@@ -23,16 +23,50 @@ function mudarMenuSuspenso(){
     
     if (menu.classList == "card indice"){
         
-        menu.classList.add("ativo")
-        botaoMenu.classList.add("ativo")
+        abrirMenu()
                 
     }else if (menu.classList == "card indice ativo"){
         
-        menu.classList.remove("ativo")
-        botaoMenu.classList.remove("ativo")
+        fecharMenu()
         
     }
     
+}
+
+function abrirMenu() {
+    menu.classList.add("ativo")
+    botaoMenu.classList.add("ativo")
+}
+
+function fecharMenu() {
+    menu.classList.remove("ativo")
+    botaoMenu.classList.remove("ativo")
+}
+
+function escolherArtigo(opcao) {
+    opcao.addEventListener('click', () => {
+
+        let textoOpcao = opcao.innerText
+        mudarArtigo(textoOpcao)
+        titulo.innerText = textoOpcao
+
+    })
+}
+
+function mudarArtigo(textoOpcao) {
+    secoes.forEach((secao) => {
+
+        if (secao.id == textoOpcao) {
+            secao.classList.add('ativo')
+
+        } else {
+            secao.classList.remove('ativo')
+
+        }
+
+    })
+
+    fecharMenu()
 }
 
 var botaoTema = document.querySelector('.lampada')
@@ -41,8 +75,11 @@ var botaoMenu = document.querySelector('.botaoIndice')
 
 var menu = document.querySelector('.indice')
 
-var secoes = document.querySelectorAll('section')
+var secoes = document.querySelectorAll('.artigos')
 var opcoesMenu = document.querySelectorAll('.opcaoMenu')
+var titulo = document.querySelector('#tituloArtigo')
 
 botaoTema.addEventListener('click', trocarTema)
 botaoMenu.addEventListener('click', mudarMenuSuspenso)
+
+opcoesMenu.forEach((opcao) => escolherArtigo(opcao));
