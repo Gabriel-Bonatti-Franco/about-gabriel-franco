9cfbcadbf19850348c247a5c6f4fe6144d7cf2e1
Gabriel-Bonatti-Franco <127668312+Gabriel-Bonatti-Franco@users.noreply.github.com>
2023-05-31 23:26:19 -0300

    Atualizar o script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,11 @@
 function carregamento(){
     const preload = document.querySelector('.preload')
+    
     preload.classList.add('inativo')
+
+    const page = document.querySelector('.pagina')
+
+    page.classList.remove('.
+carregando')
+
 }
